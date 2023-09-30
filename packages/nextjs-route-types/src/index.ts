e6a590b5d4bf4ecad715f881ec761629599690b2
Vu Van Dung <me@joulev.dev>
2023-09-30 22:21:38 +0700

    enable watching for changes during dev mode
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- packages/nextjs-route-types/src/index.ts
+++ packages/nextjs-route-types/src/index.ts
@@ -1,3 +1,4 @@
+import { watch } from "chokidar";
 import type { NextConfig } from "next";
 import type { Configuration, WebpackPluginInstance } from "webpack";
 
@@ -9,16 +10,38 @@ import { getAppDirectory } from "./utils";
 
 type WebpackConfigContext = Parameters<NonNullable<NextConfig["webpack"]>>[1];
 
+// https://github.com/tatethurston/nextjs-routes/blob/5ac00a885e06aa89b8eab029288d32955d7df5a6/packages/nextjs-routes/src/config.ts#L14-L23
+function debounce<Fn extends (...args: unknown[]) => unknown>(
+  fn: Fn,
+  ms: number,
+): (...args: Parameters<Fn>) => void {
+  let id: NodeJS.Timeout;
+  return (...args) => {
+    clearTimeout(id);
+    id = setTimeout(() => fn(...args), ms);
+  };
+}
+
 class NextJSRouteTypesPlugin implements WebpackPluginInstance {
   name = "NextJSRouteTypesPlugin";
   constructor(private readonly context: WebpackConfigContext) {}
+
+  async main(appDir: string) {
+    const tree = await getDirectoryTree(appDir);
+    await generateFiles(appDir, tree, getFileContent);
+  }
 
   async apply() {
     if (this.context.isServer) return;
     try {
       const appDir = await getAppDirectory();
-      const tree = await getDirectoryTree(appDir);
-      await generateFiles(appDir, tree, getFileContent);
+      if (this.context.dev) {
+        const watcher = watch(appDir, { persistent: true });
+        const generate = debounce(() => this.main(appDir), 50);
+        watcher.on("add", generate).on("unlink", generate);
+      } else {
+        await this.main(appDir);
+      }
     } catch (e) {
       if (e instanceof NextJSRouteTypesError) {
         console.error(e.message);
