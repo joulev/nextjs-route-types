ae4c2ee3891bdaeae4743661a0f310f83f1902ea
Vu Van Dung <me@joulev.dev>
2023-10-27 12:40:35 +0700

    upgrade dependencies and add nextjs 14 support
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- apps/demo/next.config.mjs
+++ apps/demo/next.config.mjs
@@ -4,6 +4,9 @@ import { withNextJSRouteTypes } from "nextjs-route-types";
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    webpackBuildWorker: true,
+  },
 };
 
 export default withNextJSRouteTypes(nextConfig);
